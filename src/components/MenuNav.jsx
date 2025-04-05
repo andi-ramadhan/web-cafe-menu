cafd91ebec01035b19c33e830a9ed9c47379c137
Andi Ramadhan <ramadhanfajar.ara@gmail.com>
2025-04-05 18:02:46 +0700

    cleaning

--- src/components/MenuNav.jsx
+++ src/components/MenuNav.jsx
@@ -13,7 +13,7 @@ const MenuNav = ({ activeCategory, setActiveCategory }) => {
   return (
     <nav className="w-full max-w-[800px] mx-auto mb-2 sm:mb-4 sm:px-4">
       <div className="flex flex-row justify-around items-center text-[var(--color-coffee-smoke)] nav-menu-container">
-        <div 
+        <div
           className="nav-menu-slider"
           style={{ transform: getSliderPosition() }}
         ></div>
